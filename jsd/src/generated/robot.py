4d4fcf29f213041de0dba7db8a84a2893773648c
Filip Frank <frank.filip@hotmail.com>
2016-02-14 02:24:54 +0100

    generating model classes with str method

--- jsd/src/generated/robot.py
+++ jsd/src/generated/robot.py
@@ -15,12 +15,66 @@ def typeDef(typeDef):
         return "CharField"
     elif typeDef == "int":
         return "IntegerField"
-    else: return typeDef 
+    elif typeDef == "bigInteger":
+        return "BigIntegerField"
+    elif typeDef == "binary":
+        return "BinaryField"
+    elif typeDef == "boolean":
+        return "BooleanField"
+    elif typeDef =="commaSeparatedInteger":
+        return "CommaSeparatedIntegerField"
+    elif typeDef =="date":
+        return "DateField"
+    elif typeDef == "dateTime":
+        return "DateTimeField"
+    elif typeDef == "decimal":
+        return "DecimalField"
+    elif typeDef == "duration":
+        return "DurationField"
+    elif typeDef == "email":
+        return "EmailField"
+    elif typeDef == "file":
+        return "FileField"
+    elif typeDef == "filePath":
+        return "FilePathField"
+    elif typeDef == "float":
+        return "FloatField"
+    elif typeDef == "image":
+        return "ImageField"
+    elif typeDef =="nullBoolean":
+        return "NullBooleanField"
+    elif typeDef == "positiveInteger":
+        return "PositiveIntegerField"
+    elif typeDef == "slug":
+        return "SlugField"
+    elif typeDef == "smallInteger":
+        return "SmallIntegerField"
+    elif typeDef == "text":
+        return "TextField"
+    elif typeDef == "time":
+        return "TimeField"
+    elif typeDef == "URL":
+        return "URLField"
+    elif typeDef == "UUID":
+        return "UUIDField"
+    elif typeDef == "foreignKey":
+        return "ForeignKey"
+    elif typeDef == "oneToOne":
+        return "OneToOneField"
+    elif typeDef == "manyToMany":
+        return "ManyToManyField"
+    
+def checkType(someitem):
+    if  ( someitem =='foreignKey' or someitem =='oneToOne' or someitem =='manyToMany'):
+        return True
+    else:
+        return False
             
 def generate(template_name, output_name, render_vars):
     env = Environment(trim_blocks = True, lstrip_blocks = True, loader = PackageLoader("generated", "templates"))
     env.filters["typeDef"] = typeDef
-
+    env.tests["checkType"] = checkType
+    
     template = env.get_template(template_name)
     rendered = template.render(render_vars)
     #i pisemo u fajl
--- jsd/src/output/models.py
+++ jsd/src/output/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 class Knjiga(models.Model):
-	naslov = models.text
-	autor = models.IntegerField
+	naslov = models.TextField()
+	autor = models.IntegerField()
+	
+	def __unicode__(self):
+		return self.naslov    
+
 class Slika(models.Model):
-	ime = models.text
-	falsifikat? = models.boolean
+	ime = models.TextField(null=True,default="ime")
+	falsifikat = models.BooleanField()
+	
+	def __unicode__(self):
+		return self.ime    
+
 class Film(models.Model):
-	naziv = models.text
-	autor = models.CharField
+	naziv = models.TextField()
+	autor = models.CharField(null=False,max_length=80)
+	knjiga = models.ForeignKey(Knjiga)
+	
+	def __unicode__(self):
+		return self.naziv    
+
