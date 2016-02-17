51744f35abba01482736ab97393f7110b1e9ee9d
Jozef Kovac <thekrushka@gmail.com>
2016-02-17 23:00:35 +0100

    added copy assets

--- jsd/src/generated/generate_django.py
+++ jsd/src/generated/generate_django.py
@@ -1,13 +1,67 @@
 import os
 
 from generated.base_generator import BaseGenerator
-from root import BASE_PATH
+from root import BASE_PATH, ROOT_DIR
 
 
 class DjangoGenerator(BaseGenerator):
     def __init__(self, model):
         BaseGenerator.__init__(self, model)
         pass
+
+    def typeDef(self, typedef):
+        if typedef == "char":
+            return "CharField"
+        elif typedef == "int":
+            return "IntegerField"
+        elif typedef == "bigInteger":
+            return "BigIntegerField"
+        elif typedef == "binary":
+            return "BinaryField"
+        elif typedef == "boolean":
+            return "BooleanField"
+        elif typedef == "commaSeparatedInteger":
+            return "CommaSeparatedIntegerField"
+        elif typedef == "date":
+            return "DateField"
+        elif typedef == "dateTime":
+            return "DateTimeField"
+        elif typedef == "decimal":
+            return "DecimalField"
+        elif typedef == "duration":
+            return "DurationField"
+        elif typedef == "email":
+            return "EmailField"
+        elif typedef == "file":
+            return "FileField"
+        elif typedef == "filePath":
+            return "FilePathField"
+        elif typedef == "float":
+            return "FloatField"
+        elif typedef == "image":
+            return "ImageField"
+        elif typedef == "nullBoolean":
+            return "NullBooleanField"
+        elif typedef == "positiveInteger":
+            return "PositiveIntegerField"
+        elif typedef == "slug":
+            return "SlugField"
+        elif typedef == "smallInteger":
+            return "SmallIntegerField"
+        elif typedef == "text":
+            return "TextField"
+        elif typedef == "time":
+            return "TimeField"
+        elif typedef == "URL":
+            return "URLField"
+        elif typedef == "UUID":
+            return "UUIDField"
+        elif typedef == "foreignKey":
+            return "ForeignKey"
+        elif typedef == "oneToOne":
+            return "OneToOneField"
+        elif typedef == "manyToMany":
+            return "ManyToManyField"
 
     @staticmethod
     def init_folder_structure(folder_list):
@@ -26,7 +80,10 @@ class DjangoGenerator(BaseGenerator):
         templates_path = os.path.join(base_path, self.model.name, 'templates')
         final_templates_path = os.path.join(templates_path, self.model.name)
         registration_path = os.path.join(templates_path, 'registration')
+
         root_html_path = templates_path
+        assets_path = os.path.join(base_path, 'assets')
+        assets_source_path = os.path.join(ROOT_DIR, 'src', 'generated', 'templates', base_source_path, 'assets')
 
         folder_gen_list = [base_path,
                            app_path,
@@ -42,6 +99,7 @@ class DjangoGenerator(BaseGenerator):
         self.generate_registration_files(base_source_path, registration_path)
         self.generate_app_files(base_source_path, app_path)
         self.generate_root_html(base_source_path, root_html_path)
+        self.copy_assets_folder(assets_source_path, assets_path)
 
     def generate_program_files(self, base_source_path, program_path):
         # program files
@@ -90,3 +148,7 @@ class DjangoGenerator(BaseGenerator):
         for e in file_gen_list:
             self.generate(base_source_path + '/program' + '/templates' + '/t{e}.tx'.format(e=e),
                           '{e}.html'.format(e=e), {'model': self.model}, root_html_path)
+
+    def copy_assets_folder(self, assets_folder, assets_path):
+        import distutils.core
+        distutils.dir_util.copy_tree(assets_folder, assets_path)
