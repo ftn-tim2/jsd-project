9c1d1b963345010e3cf4038c0383b23f3df86613
Berko Szabolcs <szberko@gmail.com>
2016-02-22 17:08:55 +0100

    added new methods to generate concrete files from templates.

--- jsd/src/generated/generate_play.py
+++ jsd/src/generated/generate_play.py
@@ -105,46 +105,118 @@ class PlayGenerator(BaseGenerator):
     def generate_application(self):
         # path to django templates
         base_source_path = os.path.join('play_templates')
-        
+        base_output_path = os.path.join(BASE_PATH, "PLAY")
+
+        # path to source folders
+        source_play_controllers = os.path.join(base_source_path, 'app', 'controllers')
+        source_play_models = os.path.join(base_source_path, 'app', 'models')
+        source_play_views = os.path.join(base_source_path, 'app', 'views')
+        source_play_views_CRUD = os.path.join(base_source_path, 'app', 'views', 'CRUD')
+        source_play_views_Secure = os.path.join(base_source_path, 'app', 'views', 'Secure')
+        source_play_views_errors = os.path.join(base_source_path, 'app', 'views', 'errors')
+        source_play_views_tags_crud = os.path.join(base_source_path, 'app', 'views', 'tags', 'crud')
+        source_play_public_javascript = os.path.join(base_source_path, 'public', 'javascript')
+        source_play_public_stylesheets = os.path.join(base_source_path, 'public', 'stylesheets')
+
         # path to the target folder
-        base_path = os.path.join(BASE_PATH, self.model.name)
-        play_class_html_path = os.path.join(base_path, self.model.name, 'play_class_html')
-        play_model_pojo_classes_path = os.path.join(base_path, self.model.name, 'play_model_pojo_classes')
-        play_controller_classes_path = os.path.join(base_path, self.model.name, 'play_controller_classes')
-        tags_play_path = os.path.join(play_class_html_path, 'tags_play')
-        folder_gen_list = [base_path,
-                           play_class_html_path,
-                           play_model_pojo_classes_path,
-                           play_controller_classes_path,
-                           tags_play_path]
+        output_play_controllers = os.path.join(base_output_path, 'app', 'controllers')
+        output_play_models = os.path.join(base_output_path, 'app', 'models')
+        output_play_views = os.path.join(base_output_path, 'app', 'views')
+        output_play_views_CRUD = os.path.join(base_output_path, 'app', 'views', 'CRUD')
+        output_play_views_Secure = os.path.join(base_output_path, 'app', 'views', 'Secure')
+        output_play_views_errors = os.path.join(base_output_path, 'app', 'views', 'errors')
+        output_play_views_tags_crud = os.path.join(base_output_path, 'app', 'views', 'tags', 'crud')
+        output_play_public_javascript = os.path.join(base_output_path, 'public', 'javascript')
+        output_play_public_stylesheets = os.path.join(base_output_path, 'public', 'stylesheets')
+
+        folder_gen_list = [base_output_path,
+                           output_play_controllers,
+                           output_play_models,
+                           output_play_views,
+                           output_play_views_CRUD,
+                           output_play_views_Secure,
+                           output_play_views_errors,
+                           output_play_views_tags_crud,
+                           output_play_public_javascript,
+                           output_play_public_stylesheets]
 
         # create the folders
         self.init_folder_structure(folder_gen_list)
-        self.generate_play_class_html(base_source_path, play_class_html_path)
-        self.generate_tags_play(base_source_path, tags_play_path)
-        self.generate_play_model_bean_classes(base_source_path, play_model_pojo_classes_path)
-        self.generate_play_controller_classes(base_source_path, play_controller_classes_path)
+        self.generate_play_view_CRUD(source_play_views_CRUD, output_play_views_CRUD)
+        self.generate_play_view_secure(source_play_views_Secure, output_play_views_Secure)
+        self.generate_play_view_errors(source_play_views_errors, output_play_views_errors)
+        self.generate_play_view_tags_crud(source_play_views_tags_crud, output_play_views_tags_crud)
+        self.generate_play_view(source_play_views, output_play_views)
+        self.generate_play_models(source_play_models, output_play_models)
+        self.generate_play_controllers(source_play_controllers, output_play_controllers)
+        self.generate_play_public_javascript(source_play_public_javascript, output_play_public_javascript)
+        self.generate_play_public_stylesheet(source_play_public_stylesheets, output_play_public_stylesheets)
 
-    def generate_play_class_html(self, base_source_path, play_class_html_path):
+    def generate_play_view_CRUD(self, source, output):
         # list of template files
-        file_gen_list = {'login_play', 'logout_play', 'change_password_play', 'list' , 'index', 'layout', 'create_Page', 'show', '404', '500' }
+        file_gen_list = {'list' , 'index', 'layout', 'createPage', 'show'}
 
         # generate the template files
         for e in file_gen_list:
-            self.generate(base_source_path + '/templates' + '/play_class_html' + '/t{e}.tx'.format(e=e),
-                          '{e}.html'.format(e=e), {'model': self.model}, play_class_html_path)    
+            self.generate(source + '/t{e}.tx'.format(e=e),
+                          '{e}.html'.format(e=e), {'model': self.model}, output)
 
-    def generate_tags_play(self, base_source_path, tags_play_path):
-        # registration files
+    def generate_play_view_secure(self,source, output):
+        # list of template files
+        file_gen_list = {'login'}
+
+        # generate the template files
+        for e in file_gen_list:
+            self.generate(source + '/t{e}.tx'.format(e=e),
+                          '{e}.html'.format(e=e), {'model': self.model}, output)
+
+    def generate_play_view_errors(self,source, output):
+        # list of template files
+        file_gen_list = {'404', '500'}
+
+        # generate the template files
+        for e in file_gen_list:
+            self.generate(source + '/t{e}.tx'.format(e=e),
+                          '{e}.html'.format(e=e), {'model': self.model}, output)
+
+    def generate_play_view_tags_crud(self,source, output):
+        # list of template files
         file_gen_list = {'checkField', 'custom', 'dateField', 'enumField', 'fileField', 'form', 'hiddenField',
                          'longtextField', 'navigation', 'numberField', 'pagination', 'passwordField', 'relationField',
                          'search', 'serializedField', 'table', 'textField'}
 
-        # generate the basic files
+        # generate the template files
         for e in file_gen_list:
-            self.generate(base_source_path +'/templates' +  '/play_class_html' + '/tags_play' + '/t{e}.tx'.format(e=e),
-                          '{e}.html'.format(e=e), {'model': self.model}, tags_play_path)
-            
+            self.generate(source + '/t{e}.tx'.format(e=e),
+                          '{e}.html'.format(e=e), {'model': self.model}, output)
+
+    def generate_play_view(self,source, output):
+        # list of template files
+        file_gen_list = {'logout', 'changePassword'}
+
+        # generate the template files
+        for e in file_gen_list:
+            self.generate(source + '/t{e}.tx'.format(e=e),
+                          '{e}.html'.format(e=e), {'model': self.model}, output)
+
+    def generate_play_public_javascript(self,source, output):
+        # list of template files
+        file_gen_list = {'jquery-1.5.2.min'}
+
+        # generate the template files
+        for e in file_gen_list:
+            self.generate(source + '/{e}.js'.format(e=e),
+                          '{e}.js'.format(e=e), {'model': self.model}, output)
+
+    def generate_play_public_stylesheet(self,source, output):
+        # list of template files
+        file_gen_list = {'crud', 'main', 'secure'}
+
+        # generate the template files
+        for e in file_gen_list:
+            self.generate(source + '/{e}.css'.format(e=e),
+                          '{e}.css'.format(e=e), {'model': self.model}, output)
+
     def prepare_play_data_model(self):
         class PreparedClass:
             def __init__(self, name, prepared_attributes):
@@ -221,7 +293,7 @@ class PlayGenerator(BaseGenerator):
         return prepared_classes
 
 
-    def generate_play_model_bean_classes(self, base_source_path, play_model_pojo_classes_path):
+    def generate_play_models(self, source, output):
         # list of template files
         # file_gen_list = "classname.java"
 
@@ -231,11 +303,11 @@ class PlayGenerator(BaseGenerator):
         for clazz_key, clazz_value in prepared_classes.items():
             # output_file_name = file_gen_list.replace('class', definition.name)
 
-            self.generate(base_source_path + '/templates' + '/' + 'play_model_pojo_classes' + '/tmodel_play.tx',
+            self.generate(source + '/tmodel_play.tx',
                           '{classname}.java'.format(classname=clazz_key),
-                          {'clazz': clazz_value}, play_model_pojo_classes_path)
+                          {'clazz': clazz_value}, output)
 
-    def generate_play_controller_classes(self, base_source_path, play_controller_classes_path):
+    def generate_play_controllers(self, source, output):
             # list of template files
             # file_gen_list = "classname.java"
 
@@ -245,6 +317,6 @@ class PlayGenerator(BaseGenerator):
             for clazz_key, clazz_value in prepared_classes.items():
                 # output_file_name = file_gen_list.replace('class', definition.name)
 
-                self.generate(base_source_path + '/templates' + '/' + 'play_controller_classes' + '/tcontroller_play.tx',
-                              '{classname}.java'.format(classname=clazz_key),
-                              {'clazz': clazz_value}, play_controller_classes_path)
+                self.generate(source + '/tcontroller_play.tx',
+                              '{classname}s.java'.format(classname=clazz_key),
+                              {'clazz': clazz_value}, output)
