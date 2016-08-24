e67b60cc6c069757c59d05721473f7b78e6017d5
Berko Szabolcs <szberko@gmail.com>
2016-08-24 21:55:25 +0200

    bump package version number

--- jsd/setup.py
+++ jsd/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ftn-jsd',
-      version='0.2',
+      version='0.3',
       description='Generate some Django and Play code',
       url='https://github.com/ftn-tim2/jsd-project.git',
       author='FTN',
